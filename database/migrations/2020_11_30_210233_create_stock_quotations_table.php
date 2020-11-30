3fd21b2838e43946edaa255e1051abbf23ed6d85
Ivanov Andrey <stels9494@mail.ru>
2020-11-30 21:57:42 +0000

    миграции

---
+++ database/migrations/2020_11_30_210233_create_stock_quotations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockQuotationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_quotations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('stock_id');
+            $table->unsignedBigInteger('price');
+            $table->timestamp('datetime');
+
+            $table->foreign('stock_id')->references('id')->on('stocks')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_quotations');
+    }
+}
---
+++ database/migrations/2020_11_30_211807_create_command_stock_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('command_stock', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('command_id');
+            $table->unsignedBigInteger('stock_id');
+            $table->float('price')->comment('по какой цене');
+            $table->integer('count')->comment('кол-во акций. если < 0 - была продажа, если > 0 - была покупка');
+            $table->timestamps();
+
+            $table->foreign('command_id')->references('id')->on('commands')->onDelete('cascade')->onDelete('cascade');
+            $table->foreign('stock_id')->references('id')->on('stocks')->onDelete('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('command_stock');
+    }
+}
