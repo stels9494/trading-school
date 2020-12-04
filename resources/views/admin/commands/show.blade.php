de543241517d25f8c914d425d49abe1221378a7f
Ivanov Andrey <stels9494@mail.ru>
2020-12-04 16:02:41 +0000

    команда для крона

--- resources/views/admin/commands/show.blade.php
+++ resources/views/admin/commands/show.blade.php
@@ -127,34 +127,33 @@
 				<h5 class="my-3">
 					Портфель команды
 				</h5>
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th scope="col">Название акции</th>
+							<th class="text-center" scope="col">Кол-во</th>
+						</tr>
+					</thead>
+					<tbody>
+
+						@foreach ($data['command']->stocks as $stock)
+							<tr>
+								<td>{{ $stock->stock->name }}</td>
+								<td class="text-center">{{ $stock->count }}</td>
+							</tr>
+						@endforeach
+
+						@if (!$data['command']->stocks->count())
+							<tr>
+								<td colspan="5" class="text-center">
+									<span class="h6 ">Портфель пустой</span>
+								</td>
+							</tr>
+						@endif
+
+					</tbody>
+				</table>
 			</div>
-
-			<table class="table table-striped">
-				<thead>
-					<tr>
-						<th scope="col">Название акции</th>
-						<th class="text-center" scope="col">Кол-во</th>
-					</tr>
-				</thead>
-				<tbody>
-
-					@foreach ($data['command']->stocks as $stock)
-						<tr>
-							<td>{{ $stock->stock->name }}</td>
-							<td class="text-center">{{ $stock->count }}</td>
-						</tr>
-					@endforeach
-
-					@if (!$data['command']->stocks->count())
-						<tr>
-							<td colspan="5" class="text-center">
-								<span class="h6 ">Портфель пустой</span>
-							</td>
-						</tr>
-					@endif
-
-				</tbody>
-			</table>
 
 		</div>
 
